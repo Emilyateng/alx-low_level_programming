08f0beee62932ed9f0d6dc0867ab83f61d4c6a2f
Emilyateng <emilyateng9@gmail.com>
2022-10-15 23:45:16 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ *  main - Entry Point
  *
- * Return : Always 0 (success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
-
+{
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+}
+
+
+
+
 
